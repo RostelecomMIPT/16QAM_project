e226307a64d6b5ce5adb137bfffc9537fb8bc11e
Артем <arktik940121@mail.ru>
2016-09-27 13:18:49 +0400

    Автокоррелятор с костылями

--- FuncCorrelation.m
+++ FuncCorrelation.m
@@ -1,10 +1,10 @@
 function [ FunctionOfCorrelation ] = FuncCorrelation( SignalOut, Nfft )
-    for k = 1 : (length(SignalOut) - Nfft - Nfft/8)
-        MedFunction = 0;
-        for l = 1 : (Nfft/8)
-            MedFunction = MedFunction +...
-                SignalOut(k + l - 1)*SignalOut(k + l + Nfft - 1);
-        end
-        FunctionOfCorrelation(k) = MedFunction;
-    end
+b=zeros(1,length(SignalOut)-1024);
+SignalOut1=[SignalOut zeros(1,100000)]; 
+   for k=0:127+length(SignalOut)-1152
+       for l=1:Nfft/8
+           b(k+1)= b(k+1)+SignalOut1(k+l)*SignalOut1(k+Nfft+l);
+       end
+   end
+   FunctionOfCorrelation=b;
 end
